52190f404d12b770a957a0acbbf9fe4720e8d4fb
bguilas <bryanjguilas@gmail.com>
2020-10-17 12:49:38 -0700

    added line to clear search field

--- app.js
+++ app.js
@@ -2,23 +2,30 @@ const $btn = document.querySelector("#btn");
 const $inputSearch = document.querySelector("#inputSearch");
 
 
-
-
 $btn.addEventListener('click', function (e) {
   e.preventDefault();
   const $userInput = $inputSearch.value.trim().toLowerCase();
-  console.log($userInput);
+  //  clears search field
+  document.querySelector('#inputSearch').value = '';
+
+
   // API CALL
   const baseUrl = (`https://pokeapi.co/api/v2/pokemon/${$userInput}/`);
+
   $.ajax({
     url: baseUrl,
     method: 'GET',
   }).then(function (response) {
     const image = response.sprites.front_default;
     const name = response.name;
+    console.log(image);
+    console.log(name);
 
   }).then(function (error) {
-    console.log(error);
+    if (error) {
+      console.log(error);
+    }
+
   });
 
 })
